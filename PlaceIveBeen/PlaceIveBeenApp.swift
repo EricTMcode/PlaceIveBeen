64f7a447474a4046f51cf693a36c579a9758c710
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-01 13:54:37 +0200

    Initial Commit

---
+++ PlaceIveBeen/PlaceIveBeenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceIveBeenApp.swift
+//  PlaceIveBeen
+//
+//  Created by Eric on 01/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct PlaceIveBeenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
